fad845613e78c805740605d71a7a5a8473d410c5
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-06-06 16:08:15 -0300

    Adding functions and finishing the app

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbol = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+const allChars = upperCase + lowerCase + numbers + symbol;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (lenght > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
